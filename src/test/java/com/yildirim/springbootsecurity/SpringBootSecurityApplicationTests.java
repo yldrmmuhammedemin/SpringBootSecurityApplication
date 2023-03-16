dfc9b494ff02859e6be415f0a49e700ea2aa58c3
Muhammed Emin Yildirim <yldrmmuhammedemin@gmail.com>
2023-03-16 13:49:15 +0300

    Spring boot security initial commit.

---
+++ src/test/java/com/yildirim/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yildirim.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
